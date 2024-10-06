7ee567f57bd4cb5a9d0ef5cca98d27710d27b1a0
adnan <a.gul@nizek.com>
2024-10-06 19:30:02 +0500

    leetcode

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -2,7 +2,7 @@ export const socialLink={
     linkenIn:"https://www.linkedin.com/in/adnangul/",
     gitHub:"https://github.com/AdnanGulzar",
     whatsApp:"https://api.whatsapp.com/send?phone=923059361889",
-    leetCode:"https://api.whatsapp.com/send?phone=923059361889",
+    leetCode:"https://leetcode.com/u/adnangul9900/",
     email:"adnan.gull548922@gmail.com",
     phone:"923059361889",
     toEmail:"mailto:adnan.gull548922@gmail.com",
