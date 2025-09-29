a0802e13a673402e703229cc56d01a5fb444c3a7
Adnan Gul <a.gul@nizek.com>
2025-09-29 09:55:18 +0500

    center the gave boxes and upate cv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -130,15 +130,15 @@ export default function Home() {
         </div>
         <div >
 
-        <button className="bg-blue hover:opacity-70 font-bold py-2 px-4 rounded ">
+        <button className=" mt-4 bg-blue hover:opacity-70 font-bold py-2 px-4 rounded ">
         <a
-            href="/adnangulfrontenddev.pdf"
-            download="adnangul.pdf"
+            href="/adnangul_mern_dev.pdf"
+            download="adnangul_mern_dev.pdf"
             className='inline-flex items-center'
           >
            
   <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-  <span>Download CV</span>
+  <span className='mt-1'>Download CV</span>
   </a>
 </button>
         </div>
